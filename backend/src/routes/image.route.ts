893cca935b6802580ab9a8e18fe802e7a800beef
Camilla Ridesjö <camilla.ridesjo@gmail.com>
2023-04-20 11:25:30 +0200

    Adjustments

--- backend/src/routes/image.route.ts
+++ backend/src/routes/image.route.ts
@@ -1,5 +1,3 @@
-//const express = require('express');
-//const router = express.Router();
 import { Router } from 'express';
 const imageRouter = Router();
 import {Request, Response} from 'express';
@@ -10,16 +8,12 @@ imageRouter.post('/create', async function(req: Request, res: Response) {
     const colors: string[] = req.body.colors;
 
     const isStrings = (arr: string[]) => arr.every(i => typeof i === 'string')
-    console.log(isStrings(colors));
-    
-
+   
     if (isStrings(colors) && colors.length > 1 && colors.length < 5) {
        
     const image = createImage(req.body.colors);
 
     const newImage = await Image.create(image);
-
-    //const newImage = await Image.create(image) if (newImage) { res.status(201) } else { res.status(500) throw new Error('fel') }
 
     try {
         const response = await newImage.save();            
