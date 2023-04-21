94bc86f7c5c76db478ff2119def0c084e42f943e
AcclaimedAP <alexander.w.pressfelt@gmail.com>
2023-04-21 17:34:32 +0200

    Removed commented out reference code

--- backend/src/controllers/game.controller.ts
+++ backend/src/controllers/game.controller.ts
@@ -10,8 +10,6 @@ type player = {
 
 
 export const createGame = asyncHandler(async (req: Request, res: Response): Promise<void> => {
-    //const testGame = new Game({ players: ["Tester", "Tester2", "Tester3"]})
-    //await testGame.save();
     const player: player = {
         name: "req.body.name",
         color: "#000000"
@@ -22,14 +20,14 @@ export const createGame = asyncHandler(async (req: Request, res: Response): Prom
     if (!result) {
         result = new Game({});
     }
-    if (result.players.length < 3) { 
+    if (result.players.length < 3) {
         result.players.push(player);
         console.log("Adding player");
         await result.save()
             .then(savedDoc => {
-            console.log(savedDoc);
-            res.send(savedDoc);
-        })
+                console.log(savedDoc);
+                res.send(savedDoc);
+            })
         
         
     } else {
@@ -40,48 +38,12 @@ export const createGame = asyncHandler(async (req: Request, res: Response): Prom
         result.active = false;
         await result.save()
             .then(savedDoc => {
-            console.log(savedDoc);
-            res.send(savedDoc);
-        })
+                console.log(savedDoc);
+                res.send(savedDoc);
+            })
         
     }
     
     
     
-    /*
-    const image: squareType = {
-        squares: {}
-    }
-
-    for (let i:number = 0; i <225; i++) {
-        const colorAmount = req.body.colors.length;
-        const colorIndex = Math.floor(Math.random()*colorAmount);         
-        image.squares[i] = req.body.colors[colorIndex]            
-    }         
-
-    const colors: string[] = req.body.colors;
-
-    const isStrings = (arr: string[]) => arr.every(i => typeof i === 'string')
-
-    if (isStrings(colors) && colors.length > 1 && colors.length < 5) {
-       
-    const newImage = await Image.create(image);
-
-    try {
-        const response = await newImage.save();            
-        res.status(200).json({message: 'image saved in database', image: {
-            id: response._id,
-            squares: response.squares
-        }})
-
-    } catch (error) {
-        res.status(500).json({message: 'internal server error', error: error})
-    }  
-
-    }   else {
-        res.status(401).json({message: 'invalid data', body: req.body})
-    }
-*/
-}
-
-)
+})
