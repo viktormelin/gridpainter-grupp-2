3eb66e445569397f52ae2180c2aa30a54c77809b
hannaforssell <forssellhanna@gmail.com>
2023-04-21 10:00:37 +0200

    Added draw interface

--- frontend/src/draw.ts
+++ frontend/src/draw.ts
@@ -1,4 +1,5 @@
 import { io } from "socket.io-client";
+import { IServerDraw } from "./models/IServerDraw";
 const socket = io("http://localhost:3000");
 
 export function createGameHTML() {
@@ -30,7 +31,7 @@ export function createGameHTML() {
 	main.appendChild(boardTable);
 }
 
-socket.on("draw", (arg) => {
+socket.on("draw", (arg: IServerDraw) => {
 	let boardTd = document.getElementById(arg.id) as HTMLTableCellElement;
 
 	boardTd.style.background = arg.color;
---
+++ frontend/src/models/IServerDraw.ts
@@ -0,0 +1,5 @@
+export interface IServerDraw {
+	id: string,
+	color: string,
+	timestamp: Date
+}
