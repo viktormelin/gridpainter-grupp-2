c4abc6f307385b5abb9896f58103c0f65dddcf09
hannaforssell <forssellhanna@gmail.com>
2023-04-21 13:49:17 +0200

    Added variable for board size

--- frontend/src/draw.ts
+++ frontend/src/draw.ts
@@ -2,6 +2,9 @@ import { io } from "socket.io-client";
 import { IServerDraw } from "./models/IServerDraw";
 const socket = io("http://localhost:3000");
 
+const boardSizeX = 15;
+const boardSizeY = 15;
+
 export function createGameHTML() {
 	let loggedInUser = sessionStorage.getItem('name');
 
@@ -9,11 +12,11 @@ export function createGameHTML() {
 	boardTable.className = "board";
 	boardTable.id = "bigBoard"
 
-	for (let i = 1; i <= 15; i++) {
+	for (let i = 0; i < boardSizeY; i++) {
 		let boardTr = document.createElement("tr")
 		boardTr.className = "new-tr";
 
-		for (let j = 1; j <= 15; j++) {
+		for (let j = 0; j < boardSizeX; j++) {
 			let boardTd = document.createElement("td")
 			boardTd.className = "new-td";
 			boardTd.id = JSON.stringify(i) + ":" + JSON.stringify(j);
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,4 +1,7 @@
 import "./style/style.scss"
 import { createLoginHTML } from "./login";
+import { createGameHTML } from "./draw";
 
 createLoginHTML();
+
+createGameHTML()
