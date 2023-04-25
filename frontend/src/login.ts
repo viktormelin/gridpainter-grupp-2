31624f9fcf7442fd1366d9184788a052cd77548f
dough <viktor.melin@me.com>
2023-04-25 10:13:22 +0200

    fetch or save user on login

--- frontend/src/login.ts
+++ frontend/src/login.ts
@@ -1,21 +1,45 @@
-import { createGameSelectHTML } from "./game-select";
+import { createGameSelectHTML } from './game-select';
+import { IUser } from './models/IUser';
+
+const isUserLoggedIn = () => {
+  const data = sessionStorage.getItem('user');
+
+  if (data) {
+    return JSON.parse(data) as IUser;
+  }
+
+  return null;
+};
 
 export function createLoginHTML() {
+  const user = isUserLoggedIn();
+
+  if (user) {
+    sessionStorage.setItem('user', JSON.stringify(user));
+    createGameSelectHTML();
+  } else {
     const main = document.querySelector('main') as HTMLElement;
 
     main.innerHTML = `
-    <form class="loginForm">
-        <input aria-label="name inputfield" type="text" id="inputUsername" placeholder="Enter name">
-        
-        <button id="submitUsername">Login</button>
-    </form>
-    `;
+          <form class="loginForm">
+              <input aria-label="name inputfield" type="text" id="inputUsername" placeholder="Enter name">
+              
+              <button id="submitUsername">Login</button>
+          </form>
+          `;
 
     const submitBtn = document.getElementById('submitUsername') as HTMLButtonElement;
     const usernameInput = document.getElementById('inputUsername') as HTMLInputElement;
     submitBtn.addEventListener('click', function (e) {
-        e.preventDefault();
-        sessionStorage.setItem('name', usernameInput.value);
-        createGameSelectHTML();
-    })
+      e.preventDefault();
+
+      const user: IUser = {
+        _id: '643fdc00090040ae33fb8ff2',
+        username: usernameInput.value,
+      };
+
+      sessionStorage.setItem('user', JSON.stringify(user));
+      createGameSelectHTML();
+    });
+  }
 }
