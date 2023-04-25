96739b671a89752207042610071f177a33c43f31
Camilla Ridesjö <camilla.ridesjo@gmail.com>
2023-04-25 12:14:41 +0200

    Added route to get one or more users.

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -4,6 +4,7 @@ import User from '../models/UserModel';
 export const addUser = asyncHandler(async (req, res) => {
   try {
     const userExists = await User.find(req.body)
+    
     if (userExists.length === 0) {
       const savedUser = await User.create(req.body)
       res.status(200).json(savedUser)
@@ -15,3 +16,16 @@ export const addUser = asyncHandler(async (req, res) => {
   }  
 });
 
+export const getUsers = asyncHandler(async (req, res) => {
+  try {
+    const usersExists = await User.find({_id: {$in:req.body.users}})
+
+    if (usersExists.length > 0 && usersExists.length === req.body.users.length) {
+      res.status(200).json(usersExists)
+    } else {
+      res.status(401).json({message: 'one or more users are not found'})
+    }  
+  } catch (error) {
+    res.status(500).json({message: 'internal server error', error: error})
+  }  
+});
