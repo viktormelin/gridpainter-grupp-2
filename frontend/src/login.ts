766c7bad8c331c187dc760b9cbf844dd7112bab3
dough <viktor.melin@me.com>
2023-04-25 10:23:41 +0200

    login input validation

--- frontend/src/login.ts
+++ frontend/src/login.ts
@@ -23,23 +23,36 @@ export function createLoginHTML() {
     main.innerHTML = `
           <form class="loginForm">
               <input aria-label="name inputfield" type="text" id="inputUsername" placeholder="Enter name">
-              
+              <p id="inputError"></p>
               <button id="submitUsername">Login</button>
           </form>
           `;
 
     const submitBtn = document.getElementById('submitUsername') as HTMLButtonElement;
     const usernameInput = document.getElementById('inputUsername') as HTMLInputElement;
+    const inputError = document.getElementById('inputError') as HTMLParagraphElement;
+
     submitBtn.addEventListener('click', function (e) {
       e.preventDefault();
 
-      const user: IUser = {
-        _id: '643fdc00090040ae33fb8ff2',
-        username: usernameInput.value,
-      };
+      inputError.innerText = '';
 
-      sessionStorage.setItem('user', JSON.stringify(user));
-      createGameSelectHTML();
+      if (usernameInput.value && usernameInput.value.length > 0) {
+        const re = /^[a-zA-Z0-9\s_.-]*$/;
+        if (re.test(usernameInput.value)) {
+          const user: IUser = {
+            _id: '643fdc00090040ae33fb8ff2',
+            username: usernameInput.value,
+          };
+
+          sessionStorage.setItem('user', JSON.stringify(user));
+          createGameSelectHTML();
+        } else {
+          inputError.innerText = 'Username contains invalid characters';
+        }
+      } else {
+        inputError.innerText = 'No username specified';
+      }
     });
   }
 }
