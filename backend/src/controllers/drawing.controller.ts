25574eca4d48829fc26e3488012ebcf0c69301ab
Camilla Ridesjö <camilla.ridesjo@gmail.com>
2023-04-27 10:58:21 +0200

    Added route to get drawing

--- backend/src/controllers/drawing.controller.ts
+++ backend/src/controllers/drawing.controller.ts
@@ -2,14 +2,29 @@ import { Request, Response } from 'express';
 import Drawing from '../models/DrawingModel';
 import asyncHandler from 'express-async-handler';
 
-export const addDrawing = asyncHandler(async (req: Request, res: Response): Promise<void> => {
- 
+export const addDrawing = asyncHandler(async (req: Request, res: Response): Promise<void> => { 
   if (req.body) {    
     const newDrawing = await Drawing.create(req.body);
-      
     try {
       const response = await newDrawing.save();
       res.status(200).json({ message: 'drawing saved in database', drawing: response });
+    } catch (error) {
+      res.status(500).json({ message: 'internal server error', error: error });
+    }
+  } else {
+    res.status(401).json({ message: 'invalid data', body: req.body });
+  }
+});
+
+export const getDrawing = asyncHandler(async (req: Request, res: Response): Promise<void> => { 
+  if (req.body.drawingId) {              
+    try {
+      const newDrawing = await Drawing.findById(req.body.drawingId);
+      if (newDrawing) {
+        res.status(200).json(newDrawing);
+      } else {
+        res.status(401).json({message: 'no drawing found'});
+      }
     } catch (error) {
       res.status(500).json({ message: 'internal server error', error: error });
     }
--- backend/src/routes/drawing.route.ts
+++ backend/src/routes/drawing.route.ts
@@ -1,9 +1,9 @@
 import { Router } from 'express';
-import { addDrawing } from '../controllers/drawing.controller';
+import { addDrawing, getDrawing } from '../controllers/drawing.controller';
 
 const drawingRouter = Router();
 
 drawingRouter.post('/add', addDrawing);
-// drawingRouter.post('/get', getDrawing);
+drawingRouter.post('/get', getDrawing);
 
 export default drawingRouter;
